0bde9306a45aeb20320235d888a5fa1b50d66314
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-17 16:50:45 +0300

    Added files via upload

---
+++ python-script/freeradius_clients.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env Python
+#-*- coding: utf-8 -*-
+
+#########################################################
+#     FREERADIUS CLIENTS SETTINGS - GH0ST S0FTWARE      #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#                  Twitter : @RTiwit                    #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+
+import os
+import sys
+import math
+import socket
+
+freeradius_clients_settings = """
+#########################################################
+#     FREERADIUS CLIENTS SETTINGS - GH0ST S0FTWARE      #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#                  Twitter : @RTiwit                    #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+"""
+print freeradius_clients_settings
+os.system("/etc/freeradius/clients.conf")
+nas_clients_ip = raw_input("Clients ip adresini giriniz : ")
+secret_name = raw_input("Secretname adını giriniz : ")
+shortname_name = raw_input("Shortname adını giriniz : ")
+clients_file = open("/etc/freeradius/clients.conf", "w")
+clients_file.write("client" + " " + nas_clients_ip + " " + "{" + "\n" + "        " + "secret" + "          " + "=" + " " + secret_name + "\n" + "        " + "shortname" + "       " + "="+ " " + shortname_name + "\n" + "}")
+loading = "Clients settings saved... ( Loading... %100 )"
+print loading
